fd77e1fe90d226a50487e2a27fcba1969bdb5d67
Caden Claussen <caden.claussen@gmail.com>
2020-06-02 19:13:51 -0700

    Formatting updates

--- prime.py
+++ prime.py
@@ -1,5 +1,11 @@
+import sys
+
+
+primeNumbers =  [ 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997 ]
+
+
 def isPrime(number):
-    for primeNumber in [ 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97 ]:
+    for primeNumber in primeNumbers:
         if primeNumber == number:
             return True
         if number % primeNumber == 0:
@@ -8,25 +14,37 @@ def isPrime(number):
 
 
 def getPrimeFactors(number):
+
     primeFactors = []
-    print(number)
-    numberOfSpaces = 0
-    for primeNumber in [ 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]:
+    print()
+    print("  ", number)
+    spaces = 1
+    for primeNumber in primeNumbers:
         while number % primeNumber == 0:
-            print(" " * numberOfSpaces, "/ \\")
-            print(" " * numberOfSpaces, primeNumber, number // primeNumber)
             number = number // primeNumber
             primeFactors.append(primeNumber)
-            numberOfSpaces = numberOfSpaces + 2
+            if (number != 1):
+                print(" " * (spaces + 1), "/ ", "\\")
+                print(" " * spaces, primeNumber, "  ", number)
+                spaces += len(str(primeNumber)) + 2
+    print()
     return primeFactors
 
 
-number = input("Type in a number: ")
+# Get the number to test
+number = sys.argv[1]
+# number = input("Type in a number: ")
+
+
+# Determine if the number is prime
 number = int(number)
 isPrime = isPrime(number)
 if isPrime == False:
     print(number, "is not prime")
 else:
     print(number, "is prime")
-factors = getPrimeFactors(number)
-print(factors)
+
+
+# Get the number's prime factors
+primeFactors = getPrimeFactors(number)
+print("Prime factors:", primeFactors)
