9a488cfe8b613954de510f88439d6185b3b73fd1
Caden Claussen <caden.claussen@gmail.com>
2020-06-02 18:39:52 -0700

    added all the prime numbers to fuction isPrime

--- prime.py
+++ prime.py
@@ -1,5 +1,5 @@
 def isPrime(number):
-    for primeNumber in [ 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37 ]:
+    for primeNumber in [ 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97 ]:
         if primeNumber == number:
             return True
         if number % primeNumber == 0:
