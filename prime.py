49646153a52ddf83ca6a50eb59cfdab235ab06e5
Caden Claussen <caden.claussen@gmail.com>
2020-06-02 17:36:50 -0700

    finished getPrimeFactors

--- prime.py
+++ prime.py
@@ -8,11 +8,13 @@ def isPrime(number):
 
 
 def getPrimeFactors(number):
-    for primeNumber in [ 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37 ]:
-        if number % primeNumber == 0:
-            return False
-    return True
-            
+    primeFactors = []
+    for primeNumber in [ 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]:
+        while number % primeNumber == 0:
+            number = number / primeNumber
+            primeFactors.append(primeNumber)
+    return primeFactors
+
 
 number = input("Type in a number: ")
 number = int(number)
@@ -21,3 +23,5 @@ if isPrime == False:
     print(number, "is not prime")
 else:
     print(number, "is prime")
+factors = getPrimeFactors(number)
+print(factors)
